141b4a620d9fb9b5eb790857fb3d1651194ff5bf
kheslop <kheslop@mastercontrol.com>
2020-02-10 16:36:00 -0700

    removed some unnecessary config items deleted an unused class

--- src/main/java/com/mastercontrol/rubyscraper/config/ScraperConfig.java
+++ src/main/java/com/mastercontrol/rubyscraper/config/ScraperConfig.java
@@ -8,8 +8,5 @@ public class ScraperConfig {
     public static String defaultPathToTests = "\\mastercontrol\\services\\Presentation\\tests\\gems\\mastercontrol-test-suite\\lib\\mastercontrol-test-suite\\tests\\";
     public static String testsPath = masterControlRoot + defaultPathToTests;
     public static String pathToValidationFRS = testsPath + "\\validation\\frs";
-    public static String pathToValidationURS = testsPath + "\\validation\\urs";
     public static String pathToFunctionalTests = testsPath + "\\functional";
-    public static final String productionRecordsValidation = pathToValidationFRS + "\\production_records";
-    public static final String productionRecordsFunctional = pathToFunctionalTests + "\\production_records";
 }
