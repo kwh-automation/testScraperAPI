1a40df5a7899c01f446862924e65643af55ab9f4
kwh-automation <kurtiswheslop@gmail.com>
2022-04-05 23:29:46 -0600

    Update ScraperConfig.java

--- src/main/java/com/mastercontrol/rubyscraper/config/ScraperConfig.java
+++ src/main/java/com/mastercontrol/rubyscraper/config/ScraperConfig.java
@@ -5,7 +5,7 @@ import static com.mastercontrol.rubyscraper.FileUtils.setMyMasterControlRootPath
 public class ScraperConfig {
 
     public static String masterControlRoot = String.valueOf(setMyMasterControlRootPath("C:\\QA\\"));
-    public static String defaultPathToTests = "\\mastercontrol\\services\\Presentation\\tests\\gems\\mastercontrol-test-suite\\lib\\mastercontrol-test-suite\\tests\\";
+    public static String defaultPathToTests = "insert_path";
     public static String testsPath = masterControlRoot + defaultPathToTests;
     public static String pathToValidationFRS = testsPath + "\\validation\\frs";
     public static String pathToFunctionalTests = testsPath + "\\functional";
